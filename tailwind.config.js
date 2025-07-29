bfd5b02e552a4af45144e6009ac15f6d717262db
Dhruv bansal <cybosecdhruv@gmail.com>
2025-07-30 00:42:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
